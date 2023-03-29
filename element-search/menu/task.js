54aa1856bc3e225438dba7d425bdcc0f83fa414e
OlegPustovalov <106656451+OlegPustovalov@users.noreply.github.com>
2023-03-29 21:53:49 +0300

    ДЗ Напвигационное меню в2

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,42 +1,31 @@
-let i = 0
-
-fa = function() {
-    console.log("прошел клик на Главная") 
-    return false // запрещение перехода по ссылке в ф-ии обработчике   
-}
+let m=0
+let n=0
 
 fb = function() {
     console.log("прошел клик о Компании") 
-    let k = s[2].closest("ul")
+    let k = s[m].closest("ul")
     k.classList.toggle("menu_active")
-    return false  
+    return false  // запрещение перехода по ссылке в ф-ии обработчике 
 }
 
 fc = function() {
     console.log("прошел клик Услуги") 
-    let k = s[6].closest("ul")
+    let k = s[n].closest("ul")
     k.classList.toggle("menu_active")
-    return false  
-}
-
-fe = function() {
-    console.log("прошел клик Контакты")   
-    return false
+    return false  // запрещение перехода по ссылке в ф-ии обработчике 
 }
 
 //зарегистрируйте обработчики события click на элементах с классом menu__link
 let s = document.querySelectorAll (".menu__link")
 
-console.log(s.length)
+for (let i=0; i < s.length;i++){
+    if (s[i].textContent == "О компании"){
+        s[i].onclick = fb
+        m=i+1
+    } 
 
-
-s[0].onclick = fa //клик на Главная
-s[1].onclick = fb //клик о Компании
-s[5].onclick = fc //клик Услуги
-s[8].onclick = fe //клик Контакты
-
-
-/*for (i=0; i < s.length;i++){
-    s[i].onclick = fa 
-}*/
-
+    if (s[i].textContent == "Услуги"){
+        s[i].onclick = fc
+        n=i+1
+    } 
+}
