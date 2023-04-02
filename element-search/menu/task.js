a36dff2782348a9a3201084e14f69f2f486a2a7b
OlegPustovalov <106656451+OlegPustovalov@users.noreply.github.com>
2023-04-02 21:46:59 +0300

    ДЗ доработанное в2

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,31 +1,15 @@
-let m=0
-let n=0
-
-fb = function() {
-    console.log("прошел клик о Компании") 
-    let k = s[m].closest("ul")
-    k.classList.toggle("menu_active")
-    return false  // запрещение перехода по ссылке в ф-ии обработчике 
-}
-
-fc = function() {
-    console.log("прошел клик Услуги") 
-    let k = s[n].closest("ul")
-    k.classList.toggle("menu_active")
-    return false  // запрещение перехода по ссылке в ф-ии обработчике 
+const fClickMenu = function() {
+    let element= this.nextElementSibling //поиск соседнего элемента выбранного
+    if (element.classList.contains('menu_sub') == true){
+        element.classList.toggle("menu_active")
+        return false  //запрещение перехода по ссылке в ф-ии обработчике 
+    } else {
+        return
+    }
 }
 
 //зарегистрируйте обработчики события click на элементах с классом menu__link
 let s = document.querySelectorAll (".menu__link")
-
 for (let i=0; i < s.length;i++){
-    if (s[i].textContent == "О компании"){
-        s[i].onclick = fb
-        m=i+1
-    } 
-
-    if (s[i].textContent == "Услуги"){
-        s[i].onclick = fc
-        n=i+1
-    } 
+    s[i].onclick = fClickMenu
 }
